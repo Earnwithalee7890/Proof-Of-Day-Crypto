2727938d4557d1a71af35e9f2057e71a15e13622
Proof Of Day <earnwithalee@proofofday.app>
2025-12-25 18:38:30 +0500

    chore: add 307 redirect for farcaster manifest

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const nextConfig = {
         formats: ['image/avif', 'image/webp'],
     },
     reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: '/.well-known/farcaster.json',
+                destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019b55ba-307e-cb9c-17b1-88fa0368ee07',
+                permanent: false,
+            },
+        ]
+    },
 }
 
 export default nextConfig
