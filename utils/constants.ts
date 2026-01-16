fbb703c21c60a21685bc32fad127d15998e09326
Proof Of Day <earnwithalee@gmail.com>
2026-01-17 00:40:45 +0500

    feat: add global constants

---
+++ utils/constants.ts
@@ -0,0 +1,18 @@
+export const APP_NAME = 'Proof Of Day'
+export const APP_DESCRIPTION = 'The Daily Onchain Ritual for Building Habits & Earning Rewards'
+
+export const SOCIAL_LINKS = {
+    twitter: 'https://twitter.com/proofofday',
+    farcaster: 'https://warpcast.com/proofofday',
+    github: 'https://github.com/Earnwithalee7890',
+}
+
+export const CONTRACT_ADDRESSES = {
+    checkIn: '0x60947AD600c925d7BDC7e29727D769EF3aa29f57',
+    staking: '0x...', // To be updated
+}
+
+export const QUERY_KEYS = {
+    userStats: 'userStats',
+    streak: 'streak',
+}
