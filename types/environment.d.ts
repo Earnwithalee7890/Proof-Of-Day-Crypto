12b78b442c9bb3debd497debcb53d0527044ea2d
Proof Of Day <earnwithalee@gmail.com>
2026-01-17 00:41:00 +0500

    chore: add environment type definitions

---
+++ types/environment.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: string
+        NEXT_PUBLIC_NEYNAR_API_KEY: string
+        NEXT_PUBLIC_BASE_RPC_URL?: string
+    }
+}
