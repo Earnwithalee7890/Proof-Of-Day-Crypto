7b5075fe4e94996409708004c73d8d54f1c2d82b
Proof Of Day <earnwithalee@gmail.com>
2026-01-17 00:41:30 +0500

    refactor: improve DailyLottery contract typing and comments

--- contracts/DailyLottery.ts
+++ contracts/DailyLottery.ts
@@ -1,87 +1,93 @@
 /**
  * DailyLottery Contract
  * 
- * A simple daily jackpot game.
+ * This file contains the ABI and address for the Daily Lottery smart contract.
+ * The contract allows users to enter a daily draw by paying a ticket price.
+ * 
+ * Network: Base Mainnet
  */
 
-export const DAILY_LOTTERY_ADDRESS = '0xefa7ce93217F2051B530D5BE2dF1b23c5b1dE938';
+// The deployed address of the Daily Lottery contract
+export const DAILY_LOTTERY_ADDRESS = '0xefa7ce93217F2051B530D5BE2dF1b23c5b1dE938' as const
 
+// The Application Binary Interface (ABI) for the contract
 export const DAILY_LOTTERY_ABI = [
     {
-        "inputs": [],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
+        inputs: [],
+        stateMutability: 'nonpayable',
+        type: 'constructor',
     },
     {
-        "anonymous": false,
-        "inputs": [{ "indexed": true, "internalType": "address", "name": "player", "type": "address" }],
-        "name": "Entered",
-        "type": "event"
+        anonymous: false,
+        inputs: [{ indexed: true, internalType: 'address', name: 'player', type: 'address' }],
+        name: 'Entered',
+        type: 'event',
     },
     {
-        "anonymous": false,
-        "inputs": [
-            { "indexed": true, "internalType": "address", "name": "winner", "type": "address" },
-            { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" },
-            { "indexed": false, "internalType": "uint256", "name": "round", "type": "uint256" }
+        anonymous: false,
+        inputs: [
+            { indexed: true, internalType: 'address', name: 'winner', type: 'address' },
+            { indexed: false, internalType: 'uint256', name: 'amount', type: 'uint256' },
+            { indexed: false, internalType: 'uint256', name: 'round', type: 'uint256' },
         ],
-        "name": "WinnerPicked",
-        "type": "event"
+        name: 'WinnerPicked',
+        type: 'event',
     },
     {
-        "inputs": [],
-        "name": "draw",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
+        inputs: [],
+        name: 'draw',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
     },
     {
-        "inputs": [],
-        "name": "enter",
-        "outputs": [],
-        "stateMutability": "payable",
-        "type": "function"
+        inputs: [],
+        name: 'enter',
+        outputs: [],
+        stateMutability: 'payable',
+        type: 'function',
     },
     {
-        "inputs": [],
-        "name": "getPlayers",
-        "outputs": [{ "internalType": "address[]", "name": "", "type": "address[]" }],
-        "stateMutability": "view",
-        "type": "function"
+        inputs: [],
+        name: 'getPlayers',
+        outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
+        stateMutability: 'view',
+        type: 'function',
     },
     {
-        "inputs": [],
-        "name": "getPot",
-        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-        "stateMutability": "view",
-        "type": "function"
+        inputs: [],
+        name: 'getPot',
+        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+        stateMutability: 'view',
+        type: 'function',
     },
     {
-        "inputs": [],
-        "name": "lastWinAmount",
-        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-        "stateMutability": "view",
-        "type": "function"
+        inputs: [],
+        name: 'lastWinAmount',
+        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+        stateMutability: 'view',
+        type: 'function',
     },
     {
-        "inputs": [],
-        "name": "lastWinner",
-        "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-        "stateMutability": "view",
-        "type": "function"
+        inputs: [],
+        name: 'lastWinner',
+        outputs: [{ internalType: 'address', name: '', type: 'address' }],
+        stateMutability: 'view',
+        type: 'function',
     },
     {
-        "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-        "name": "players",
-        "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-        "stateMutability": "view",
-        "type": "function"
+        inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+        name: 'players',
+        outputs: [{ internalType: 'address', name: '', type: 'address' }],
+        stateMutability: 'view',
+        type: 'function',
     },
     {
-        "inputs": [],
-        "name": "ticketPrice",
-        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-        "stateMutability": "view",
-        "type": "function"
-    }
-] as const;
+        inputs: [],
+        name: 'ticketPrice',
+        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+        stateMutability: 'view',
+        type: 'function',
+    },
+] as const
+
