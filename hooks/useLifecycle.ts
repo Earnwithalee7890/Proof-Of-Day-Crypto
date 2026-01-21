835ae9c3c41ca896acf5b37bce39a28be588668f
Proof Of Day <earnwithalee@gmail.com>
2026-01-22 03:07:59 +0500

    feat: add usePrevious and render lifecycle hooks

---
+++ hooks/useLifecycle.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+export const usePrevious = <T>(value: T): T | undefined => {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+};
+
+export const useRenderCount = (): number => {
+  const renderCount = useRef(0);
+  renderCount.current += 1;
+  return renderCount.current;
+};
+
+export const useIsFirstRender = (): boolean => {
+  const isFirst = useRef(true);
+
+  if (isFirst.current) {
+    isFirst.current = false;
+    return true;
+  }
+
+  return false;
+};
